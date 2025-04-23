d2f2ddd1e48e1ca36582c6b2cdde66ba8a14cd1e
NateUy <rodisaacnathanuy@gmail.com>
2025-04-23 00:42:56 -0400

    fix load scrolling

--- public/js/script.js
+++ public/js/script.js
@@ -13,6 +13,29 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   });
 
+  const accordions = document.querySelectorAll("details");
+
+  accordions.forEach((accordion) => {
+    accordion.addEventListener("toggle", () => {
+      let lastAccordion = accordion;
+
+      if (accordion.open) {
+        accordions.forEach((otherAccordion) => {
+          if (otherAccordion !== accordion) {
+            otherAccordion.open = false;
+          }
+        });
+      }
+      setTimeout(() => {
+        AOS.refresh();
+        lastAccordion.scrollIntoView({
+          behavior: "smooth",
+          block: "start",
+        });
+      }, 100);
+    });
+  });
+
   const menuBtn = document.getElementById("menu-btn");
   const mobileMenu = document.getElementById("mobile-menu");
   const menuLinks = document.querySelectorAll(".menu-link");
